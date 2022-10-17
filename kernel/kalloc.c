40469b729348b62f905046cf10b88a7312923371
amily-lala <1763825891@qq.com>
2022-10-17 08:14:51 +0000

    lab3:first commit:大致思路&数据结构，仅修改kalloc kfree

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,15 +18,26 @@ struct run {
   struct run *next;
 };
 
-struct {
-  struct spinlock lock;
-  struct run *freelist;
-} kmem;
+struct kmem{
+  struct spinlock lock; // multi process compete for this lock
+  struct run *freelist; // free physical page : 1.0 4kB/page ; 2.0 spinlock
+};
+
+// lab 3:
+// 实验思路：为每个CPU核分配独立的内存链表
+// 数据结构：kmems 是一个kmem数组，数组容量为NCPU (max)
+// TODO:
+// 1.0 修改相关和内存管理器有关的操作
+// 2.0 优化，减少锁争用
+struct kmem kmems[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  // TODO:STEP1 initlock
+  for (int i = 0; i < NCPU; i++) {
+    initlock(&kmems[i].lock, "kmem"+i);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +67,15 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // TODO :STEP3 优先将内存放入当前CPU的freelist中
+  push_off(); // 关中断
+  int cpuID = cpuid(); // 获取当前CPU的id
+  acquire(&kmems[cpuID].lock);
+  r->next = kmems[cpuID].freelist;
+  kmems[cpuID].freelist = r;
+  release(&kmems[cpuID].lock);
+  pop_off(); // 开中断
+
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,13 +86,41 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  // TODO:STEP2 优先为当前CPU分配内存
 
+  push_off(); // 关中断
+
+  int cpuID = cpuid(); // 获取当前CPU的id
+  acquire(&kmems[cpuID].lock);
+  r = kmems[cpuID].freelist;
+  if(r) {
+    kmems[cpuID].freelist = r->next;
+  }
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+  release(&kmems[cpuID].lock);
+
+  pop_off(); // 开中断  // 位置？？？？
   return (void*)r;
+
+  // TODO:若当前CPU没有空闲内存块，则从其他CPU的freelist窃取
+  // Q:按照for循环的顺序，还是随机一点好呢？
+  if(!r) {
+    for (int i = 0; i < NCPU; i++) {
+      if (i != cpuID) {
+        acquire(&kmems[i].lock);
+        r = kmems[i].freelist;
+        if(r) {
+          kmems[cpuID].freelist = r->next;
+        }
+        if(r)
+          memset((char*)r, 5, PGSIZE); // fill with junk
+        release(&kmems[i].lock);
+        return (void*)r;
+      }
+    }
+  }
+
+  return 0; //没有空闲内存，返回0（null）
 }
+ 
